34744947b0d248073b5eed09e1bcf6c265247d13
Елизавета Корнеева <kornea2005@mail.ru>
2023-09-12 00:41:55 +0500

    Task7

---
+++ Tyuiu.KorneevaEA.Sprint0.Task7.V0.Lib/DataServise.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.KorneevaEA.Sprint0.Task7.V0.Lib
+{
+    public class DataServise
+    {
+        
+        public static int[] AdditionArrays(int[] numOne, int[] numTwo)
+        {
+            int[] resuliArray = new int[5];
+            for (var i = 0; i < resuliArray.Length; i++)
+            {
+                resuliArray[i] = numOne[i] + numTwo[i];
+            }
+            return resuliArray;
+        }
+    }
+}
---
+++ Tyuiu.KorneevaEA.Sprint0.Task7.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KorneevaEA.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KorneevaEA.Sprint0.Task7.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2a39bce4-3b2c-4dfd-8127-5ae9a1639d1a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
