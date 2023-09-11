0eea785a5b69596808bff004abfb2336277b199a
Елизавета Корнеева <kornea2005@mail.ru>
2023-09-11 16:26:05 +0500

    Task4

---
+++ Tyuiu.KorneevaEA.Sprint0.Task4.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.KorneevaEA.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.KorneevaEA.Sprint0.Task4.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c3fa5587-885c-4613-8e91-a2ef4aa3ceed")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.KorneevaEA.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.KorneevaEA.Sprint0.Task4.V0.Lib;
+
+
+namespace Tyuiu.KorneevaEA.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(1, 5));
+
+            Console.WriteLine(DataService.Subtraction(15, 5));
+
+            Console.WriteLine(DataService.Multiplikation(10, 10));
+
+            Console.WriteLine(DataService.Division(5, 5));
+            
+            Console.ReadKey();
+        }
+    }
+}
